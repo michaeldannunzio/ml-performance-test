87a5ac8d206cb9055601810b8aec3b033fcbe8f7
michaeldannunzio <michael.dannunzio3@gmail.com>
2020-02-15 19:40:31 -0600

    completed logistic regression using armadillo library for matrix operations

--- logistic-regression/cpp/logistic-regression.cpp
+++ logistic-regression/cpp/logistic-regression.cpp
@@ -5,72 +5,64 @@
 #include <fstream>    // file streaming
 #include <cmath>      // math functions
 #include <string>     // string utilities
-#include <sstream>
 #include <armadillo>
 #include <chrono>
+
+using namespace std;
+using namespace arma;
 
 // preprocessor macros
 #define FILEPATH "../plasma.csv"
 
 // type aliases
-typedef std::map<std::string, std::vector<std::string> > Dataframe;
-
-// global variables
-Dataframe df;
+typedef map<string, vector<double> > Dataframe;
 
 // function prototypes
-arma::Mat<float> read_csv(std::string&);
-std::vector<std::string> split(const std::string&, const std::string&);
-std::string strip(const std::string&, const std::string&);
-void display(Dataframe);
+Dataframe read_csv(string&);
+vector<string> split(const string&, const string&);
+string strip(const string&, const string&);
+
+mat sigmoid(mat z);
 
 // entry point
 int main(int argc, char *argv[]) {
-  std::string s(FILEPATH);
+  string filepath(FILEPATH);
 
-  read_csv(s);
-  display(df);
+  Dataframe df = read_csv(filepath);
+
+  double learning_rate = 0.001;
+  mat weights(2, 1, fill::ones);
+  mat labels(df["ESR"]);
+  mat prob_vector(32, 1, fill::zeros);
+  mat error(32, 1);
+  mat data_matrix = join_rows(
+    mat(32, 1, fill::ones),
+    mat(df["fibrinogen"])
+  );
+
+  for (int i = 0; i < 500000; i++) {
+    prob_vector = sigmoid(data_matrix * weights);
+    error = labels - prob_vector;
+    weights = weights + learning_rate * data_matrix.t() * error;
+  }
+
+  weights.print("weights = ");
   
   return EXIT_SUCCESS;
 }
 
-void display(Dataframe df) {
-  std::vector<std::string> attrs;
-  std::string attr;
-
-  for (
-    Dataframe::iterator it = df.begin();
-    it != df.end();
-    it++
-  ) {
-    attr = it->first;
-    attrs.push_back(attr);
-    std::cout << attr << ",";
-  }
-
-  int len = df[attrs[0]].size();
-  std::cout << std::endl;
-
-  for (int i = 1; i < len; i++) {
-    for (int j = 0; j < attrs.size(); j++) {
-      std::cout << df[attrs[j]][i] << ",";
-    }
-    std::cout << std::endl;
-  }
-}
-
-arma::Mat<float> read_csv(std::string& filepath) {
-  arma::Mat<float> mat;
-  std::ifstream fin;
+Dataframe read_csv(string& filepath) {
+  Dataframe df;
+  ifstream fin;
   char buffer[256];
-  std::vector<std::string> attrs;
-  std::vector<std::string> vals;
+  vector<string> attrs;
+  vector<string> vals;
   int cols = 0;
 
   fin.open(filepath);
 
   if (!fin) {
-    std::cout << "[ERROR] Unable to open file: " << filepath << std::endl;
+    cout << "[ERROR] Unable to open file: " << filepath << endl;
     exit(EXIT_FAILURE);
   }
 
@@ -79,9 +71,7 @@ arma::Mat<float> read_csv(std::string& filepath) {
   cols = attrs.size();
 
   for (int i = 0; i < cols; i++) {
-    std::cout << attrs[i] << std::endl;
     attrs[i] = strip(attrs[i], "\"");
-    std::cout << attrs[i] << std::endl;
   }
 
   // read remaining data
@@ -89,31 +79,35 @@ arma::Mat<float> read_csv(std::string& filepath) {
     vals = split(buffer, ",");
     
     for (int i = 1; i < cols; i++) {
-      std::string current_attr = attrs[i];
+      string current_attr = attrs[i];
 
       if (current_attr == "ESR")
         vals[i] = vals[i][5] == '>' ? "1" : "0";
 
-      df[attrs[i]].push_back(strip(vals[i], "\""));
+      vals[i] = strip(vals[i], "\"");
+
+      double temp = stof(vals[i]);
+
+      df[attrs[i]].push_back(temp);
     }
   }
 
   fin.close();
 
-  return mat;
+  return df;
 }
 
-std::vector<std::string> split(const std::string& str, const std::string& delim) {
-  std::vector<std::string> tokens;
+vector<string> split(const string& str, const string& delim) {
+  vector<string> tokens;
   size_t prev = 0, pos = 0;
   
   do {
     pos = str.find(delim, prev);
-    if (pos == std::string::npos) {
+    if (pos == string::npos) {
       pos = str.length();
     }
 
-    std::string token = str.substr(prev, pos-prev);
+    string token = str.substr(prev, pos-prev);
     if (!token.empty()) {
       tokens.push_back(token);
     }
@@ -124,26 +118,17 @@ std::vector<std::string> split(const std::string& str, const std::string& delim)
   return tokens;
 }
 
-std::string strip(const std::string& str, const std::string& delim) {
-  std::vector<std::string> str_arr = split(str, delim);
+string strip(const string& str, const string& delim) {
+  vector<string> str_arr = split(str, delim);
 
-  std::string final_str = "";
+  string final_str = "";
 
-  for (std::string s : str_arr)
+  for (string s : str_arr)
     final_str += s;
 
   return final_str;
 }
 
-
-std::vector<float> sigmoid(std::vector<float> z) {
-  std::vector<float> f;
-  float fi;
-
-  for (float zi : z) {
-    fi = 1 / ( 1 + exp(zi) );
-    f.push_back(fi);
-  }
-
-  return f;
+mat sigmoid(mat z) {
+  return (1 / (1 + arma::exp(-z)));
 }
