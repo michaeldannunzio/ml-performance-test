dfbc7471ba925ed7d6b2aad7585aca127cb3dbf0
zhusain <47133159+zhusain@users.noreply.github.com>
2020-02-15 20:57:16 -0600

    Update naive-bayes.r
    Fully functional script, may need header info with our names and such

---
+++ naive-bayes/rscript/naive-bayes.r
@@ -0,0 +1,22 @@
+# Load titanic csv into R
+titanic_project <- read.csv("/Users/zainhusain/Documents/SE4375/titanic_project.csv")
+# make copy and change columns to factors
+tp <- titanic_project
+tp$survived <- as.factor(tp$survived)
+tp$sex <- as.factor(tp$sex)
+tp$pclass <- as.factor(tp$pclass)
+# create train and test sets
+train <- tp[1:900,]
+test <- tp[901:1046,]
+# install.packages("e1071")
+# load e1071 library for Naive Bayes
+library(e1071)
+# Create Naive Bayes model from train data and view model
+nb1 <- naiveBayes(survived ~ pclass+age+sex , data = train)
+nb1
+# predict on test data
+predictions_class <- predict(nb1, newdata=test, type="class")
+# predictions_raw <- predict(nb1, newdata=test, type="raw")
+# head(predictions_raw)
+# view confusion matrix and statistics
+confusionMatrix(predictions_class, test$survived, positive = "1")
