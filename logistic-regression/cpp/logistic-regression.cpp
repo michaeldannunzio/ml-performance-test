8162a8a672c0011d218e8dffcf6f4c99956eea79
michaeldannunzio <michael.dannunzio3@gmail.com>
2020-02-15 19:39:57 -0600

    linked armadillo library

--- logistic-regression/cpp/logistic-regression.cpp
+++ logistic-regression/cpp/logistic-regression.cpp
@@ -9,8 +9,10 @@
 #include <armadillo>
 #include <chrono>
 
+// preprocessor macros
 #define FILEPATH "../plasma.csv"
 
+// type aliases
 typedef std::map<std::string, std::vector<std::string> > Dataframe;
 
 // global variables
@@ -18,41 +20,52 @@ Dataframe df;
 
 // function prototypes
 arma::Mat<float> read_csv(std::string&);
+std::vector<std::string> split(const std::string&, const std::string&);
+std::string strip(const std::string&, const std::string&);
 void display(Dataframe);
 
+// entry point
 int main(int argc, char *argv[]) {
   std::string s(FILEPATH);
 
-  std::cout << std::endl << "---------------" << std::endl;
   read_csv(s);
-  std::cout << "---------------" << std::endl;
+  display(df);
   
   return EXIT_SUCCESS;
 }
 
 void display(Dataframe df) {
   std::vector<std::string> attrs;
+  std::string attr;
 
   for (
     Dataframe::iterator it = df.begin();
     it != df.end();
     it++
   ) {
-    attrs.push_back(it->first);
+    attr = it->first;
+    attrs.push_back(attr);
+    std::cout << attr << ",";
   }
 
+  int len = df[attrs[0]].size();
+  std::cout << std::endl;
 
+  for (int i = 1; i < len; i++) {
+    for (int j = 0; j < attrs.size(); j++) {
+      std::cout << df[attrs[j]][i] << ",";
+    }
+    std::cout << std::endl;
+  }
 }
 
 arma::Mat<float> read_csv(std::string& filepath) {
   arma::Mat<float> mat;
-
   std::ifstream fin;
-  std::vector<std::string> attrs;
   char buffer[256];
-  std::istringstream iss;
-  std::string value;
-  int i = 0;
+  std::vector<std::string> attrs;
+  std::vector<std::string> vals;
+  int cols = 0;
 
   fin.open(filepath);
 
@@ -61,15 +74,28 @@ arma::Mat<float> read_csv(std::string& filepath) {
     exit(EXIT_FAILURE);
   }
 
-  while (fin.get(buffer, 256, ',')) {
-    value = buffer;
-    attrs.push_back(value);
+  fin.getline(buffer, 256, '\n');  // get column names
+  attrs = split(buffer, ",");
+  cols = attrs.size();
+
+  for (int i = 0; i < cols; i++) {
+    std::cout << attrs[i] << std::endl;
+    attrs[i] = strip(attrs[i], "\"");
+    std::cout << attrs[i] << std::endl;
   }
 
-  while (fin.getline(buffer, 256, ',')) {
-    value = buffer;
-    df[attrs[i]].push_back(value);
-    i++;
+  // read remaining data
+  while (fin.getline(buffer, 256, '\n')) {
+    vals = split(buffer, ",");
+    
+    for (int i = 1; i < cols; i++) {
+      std::string current_attr = attrs[i];
+
+      if (current_attr == "ESR")
+        vals[i] = vals[i][5] == '>' ? "1" : "0";
+
+      df[attrs[i]].push_back(strip(vals[i], "\""));
+    }
   }
 
   fin.close();
@@ -77,23 +103,47 @@ arma::Mat<float> read_csv(std::string& filepath) {
   return mat;
 }
 
-std::vector<std::string> split(const std::string& line, const std::string& delimiter) {
+std::vector<std::string> split(const std::string& str, const std::string& delim) {
   std::vector<std::string> tokens;
   size_t prev = 0, pos = 0;
   
   do {
-    pos = line.find(delimiter, prev);
+    pos = str.find(delim, prev);
     if (pos == std::string::npos) {
-      pos = line.length();
+      pos = str.length();
     }
 
-    std::string token = line.substr(prev, pos-prev);
+    std::string token = str.substr(prev, pos-prev);
     if (!token.empty()) {
       tokens.push_back(token);
     }
     
-    prev = pos + delimiter.length();
-  } while (pos < line.length() && prev < line.length());
+    prev = pos + delim.length();
+  } while (pos < str.length() && prev < str.length());
   
   return tokens;
+}
+
+std::string strip(const std::string& str, const std::string& delim) {
+  std::vector<std::string> str_arr = split(str, delim);
+
+  std::string final_str = "";
+
+  for (std::string s : str_arr)
+    final_str += s;
+
+  return final_str;
+}
+
+
+std::vector<float> sigmoid(std::vector<float> z) {
+  std::vector<float> f;
+  float fi;
+
+  for (float zi : z) {
+    fi = 1 / ( 1 + exp(zi) );
+    f.push_back(fi);
+  }
+
+  return f;
 }
