5348efad36d491a63aaa343e0c3454a4442f7b72
zhusain <47133159+zhusain@users.noreply.github.com>
2020-02-16 19:09:26 -0600

    added timing -- finished
    timing starts before model is built and ends after statistics are printed

--- naive-bayes/naive-bayes.r
+++ naive-bayes/naive-bayes.r
@@ -6,7 +6,7 @@
 ###################################################################
 
 # Load titanic csv into R
-titanic_project <- read.csv("../titanic_project.csv")
+titanic_project <- read.csv("/Users/zainhusain/Documents/SE4375/titanic_project.csv")
 
 # make copy and change columns to factors
 tp <- titanic_project
@@ -22,18 +22,20 @@ test <- tp[901:1046,]
 # install.packages("e1071")
 library(e1071)
 
-# Create Naive Bayes model from train data and view model
-nb1 <- naiveBayes(survived ~ pclass+age+sex , data = train)
-nb1
-
-# predict on test data
-predictions_class <- predict(nb1, newdata=test, type="class")
-
 # variables for calculating metrics
 tp <- 0
 tn <- 0
 fp <- 0
 fn <- 0
+
+# start timing
+start <- proc.time()
+
+# Create Naive Bayes model from train data and view model
+nb1 <- naiveBayes(survived ~ pclass+age+sex , data = train)
+
+# predict on test data
+predictions_class <- predict(nb1, newdata=test, type="class")
 
 # predictions as integer vector
 predVec <- as.integer(predictions_class)
@@ -44,15 +46,24 @@ for (x in 1:146)
   if (predVec[x] == 1) {if (test$survived[x] == 0) tn <- tn + 1 else fn <- fn + 1}
   if (predVec[x] == 2) {if (test$survived[x] == 1) tp <- tp + 1 else fp <- fp + 1}
 }
-                        
-# calculate metrics
+
+# Results of confusion matrix
+print(paste("TP: ", tp))
+print(paste("TN: ", tn))
+print(paste("FP: ", fp))
+print(paste("FN: ", fn))
+
+# calculate and print metrics
 ac <- ((tp + tn) / nrow(test))
 print(paste("Accuracy: ", ac))
 se <- (tp / (tp + fn))
 print(paste("Sensitivity: ", se))
 sp <- (tn / (tn + fp))
 print(paste("Specificity: ", sp))
-                        
-# view confusion matrix and statistics
-confusionMatrix(predictions_class, test$survived, positive = "1")
 
+# stop timing and print time elapsed
+print(proc.time() - start)
+
+# view model, confusion matrix, and statistics
+nb1
+confusionMatrix(predictions_class, test$survived, positive = "1")
